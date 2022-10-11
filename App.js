ce9d3c863a59eaf3d89fd23572f23a0eb49d35ad
Kacper <kacper.siembab@gmail.com>
2022-10-11 18:32:41 +0200

    prank

--- App.js
+++ App.js
@@ -1,20 +1,11 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function App() {
 	return (
-		<View style={styles.container}>
-			<Text>Open up App.js to start working on your ap p!</Text>
+		<View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
 			<StatusBar style="auto" />
+			<Text>Ale was sprankowałem</Text>
 		</View>
 	);
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#fff",
-		alignItems: "center",
-		justifyContent: "center",
-	},
-});
