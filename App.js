88dd0a55d3f440aa6cbbdd11434cc34b78a0c419
Kacper <kacper.siembab@gmail.com>
2022-10-13 14:27:56 +0200

    added tab navigation

--- App.js
+++ App.js
@@ -1,15 +1,28 @@
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import NextWasteCard from "./src/components/NextWasteCard/NextWasteCard";
 import WasteCard from "./src/components/WasteCard/WasteCard";
+import WasteScreen from "./src/screens/WasteScreen/WasteScreen";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
 	return (
-		<View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-			<StatusBar style="auto" />
-			<Text>Ale was sprankowałem</Text>
-			<NextWasteCard wasteDate={"21.07.2024"} wasteType={"Plastik"} />
-			<WasteCard />
-		</View>
+		<NavigationContainer>
+			<StatusBar style="light" />
+			<Tab.Navigator
+				screenOptions={{
+					headerShown: false,
+					tabBarStyle: { backgroundColor: "#1B4332", borderTopWidth: 0 },
+					tabBarInactiveTintColor: "#D8F3DC",
+					tabBarActiveTintColor: "#95D5B2"
+				}}
+			>
+				<Tab.Screen name="Wywóz Odpadów" component={WasteScreen} />
+				<Tab.Screen name="Next-Waste2" component={WasteCard} />
+			</Tab.Navigator>
+		</NavigationContainer>
 	);
 }
