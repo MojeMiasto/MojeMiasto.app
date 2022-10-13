0023cba9ccb9261c2828b01968f18d75582f30a9
Kacper <kacper.siembab@gmail.com>
2022-10-13 14:27:18 +0200

    added a default styles file

---
+++ styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Dimensions } from "react-native";
+
+export const defaultBorderRadius = 16;
+
+export default StyleSheet.create({
+	wrapper: {
+		flex: 1,
+		alignItems: "center",
+		padding: 16,
+		backgroundColor: "#081C15"
+	}
+});
