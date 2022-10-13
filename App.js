1efe4bbedd221f3218680b8008387ad565bfdf7f
Kacper <kacper.siembab@gmail.com>
2022-10-13 20:23:01 +0200

    added icons and refactored code

--- App.js
+++ App.js
@@ -1,27 +1,44 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "@expo/vector-icons/Ionicons";
+
 import NextWasteCard from "./src/components/NextWasteCard/NextWasteCard";
 import WasteCard from "./src/components/WasteCard/WasteCard";
 import WasteScreen from "./src/screens/WasteScreen/WasteScreen";
 
+import { colors } from "./styles";
+
 const Tab = createBottomTabNavigator();
+
+const screenOptions = ({ route }) => ({
+	headerShown: false,
+	tabBarIcon: ({ focused, color, size }) => {
+		let iconName;
+
+		if (route.name === "Waste") {
+			iconName = focused ? "ios-trash" : "ios-trash-outline";
+		} else if (route.name === "Awarie") {
+			iconName = focused ? "ios-warning" : "ios-warning-outline";
+		}
+
+		return <Ionicons name={iconName} size={size} color={color} />;
+	},
+	tabBarStyle: {
+		backgroundColor: colors.primaryLight,
+		borderTopWidth: 0
+	},
+	tabBarInactiveTintColor: colors.text,
+	tabBarActiveTintColor: colors.accentLight
+});
 
 export default function App() {
 	return (
 		<NavigationContainer>
 			<StatusBar style="light" />
-			<Tab.Navigator
-				screenOptions={{
-					headerShown: false,
-					tabBarStyle: { backgroundColor: "#1B4332", borderTopWidth: 0 },
-					tabBarInactiveTintColor: "#D8F3DC",
-					tabBarActiveTintColor: "#95D5B2"
-				}}
-			>
-				<Tab.Screen name="Wywóz Odpadów" component={WasteScreen} />
-				<Tab.Screen name="Next-Waste2" component={WasteCard} />
+			<Tab.Navigator screenOptions={screenOptions}>
+				<Tab.Screen name="Awarie" component={WasteCard} />
+				<Tab.Screen name="Waste" component={WasteScreen} />
 			</Tab.Navigator>
 		</NavigationContainer>
 	);
