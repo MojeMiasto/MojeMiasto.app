ad8dc17640c5afe1f9a3b1f55c48844e325c62b3
ShakingDonut <kacper.siembab@gmail.com>
2022-10-20 14:30:46 +0200

    street number screen added

---
+++ src/screens/LocationStreetNumberScreen/LocationStreetNumberScreen.jsx
@@ -0,0 +1,104 @@
+import { Text, Image, ScrollView, KeyboardAvoidingView } from "react-native";
+import React, { useState, useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+import SearchBar from "../../components/SearchBar/SearchBar";
+import Background from "../../components/Background/Background";
+import LocationConfirmButton from "./LocationStreetNumberScreenConfirmButton";
+
+import defaultStyles, { screenWidth } from "../../styles";
+import { t } from "i18next";
+
+export default function LocationStreetNumberScreen({ navigation, route }) {
+	const { selectedTown, selectedStreet } = route.params;
+	const [selectedNumber, setSelectedNumber] = useState("");
+	const [searchText, setSearchText] = useState("");
+	const [numbers, setNumbers] = useState([]);
+
+	const searchTextHandler = debounce((text) => setSearchText(text));
+	const selectStreetHandler = (addr) => {
+		setSelectedNumber(addr);
+	};
+
+	function debounce(func, timeout = 500) {
+		let timer;
+		return (...args) => {
+			clearTimeout(timer);
+			timer = setTimeout(() => {
+				func.apply(this, args);
+			}, timeout);
+		};
+	}
+
+	const onPressHandler = async () => {
+		const address = numbers.filter(
+			(addr) => addr.address.houseNumber === selectedNumber
+		);
+		const addressJSON = JSON.stringify(address[0]);
+		try {
+			await AsyncStorage.setItem("userAddress", addressJSON);
+		} catch (e) {
+			console.error(e);
+		} finally {
+			// navigation
+		}
+	};
+
+	useEffect(() => {
+		const onMountDataLoad = async () => {
+			const response = await fetch(
+				`https://mojemiasto-api.azurewebsites.net/api/data/addressautocomplete?dataString=${
+					selectedTown + " " + selectedStreet + " " + searchText
+				}&type=houseNumber`
+			);
+			const data = await response.json();
+			setNumbers(data);
+		};
+		if (searchText.length > 0) {
+			onMountDataLoad();
+		} else {
+			setNumbers([]);
+		}
+	}, [searchText]);
+
+	return (
+		<Background>
+			<Image
+				source={require("../../assets/img/shakehand.png")}
+				resizeMode="contain"
+				style={{ width: screenWidth, height: 250 }}
+			/>
+			<KeyboardAvoidingView behavior={"position"}>
+				<SearchBar
+					setValue={searchTextHandler}
+					placeholder={t("location:searchNumber")}
+				/>
+
+				<ScrollView style={[defaultStyles.locationContainer]}>
+					{numbers.length > 0
+						? numbers.map((street) => (
+								<Text
+									onPress={() =>
+										selectStreetHandler(street.address.houseNumber)
+									}
+									style={[
+										defaultStyles.townItem,
+										selectedNumber == street.address.houseNumber
+											? defaultStyles.townItemFocused
+											: ""
+									]}
+									key={street.address.houseNumber}
+								>
+									{street.address.street} {street.address.houseNumber},{" "}
+									{street.address.city}
+								</Text>
+						  ))
+						: null}
+				</ScrollView>
+			</KeyboardAvoidingView>
+			{selectedNumber ? (
+				<LocationConfirmButton onPress={onPressHandler} />
+			) : null}
+		</Background>
+	);
+}
---
+++ src/screens/LocationStreetNumberScreen/styles_LocationStreetNumberScreen.js
@@ -0,0 +1,25 @@
+import { StyleSheet } from "react-native";
+import { colors, screenWidth } from "../../styles";
+
+export default StyleSheet.create({
+	title: {
+		fontFamily: "Poppins_400",
+		fontSize: 32,
+		color: colors.text
+	},
+	button_wrapper: {
+		justifyContent: "space-around",
+		alignItems: "center",
+		flexDirection: "row",
+		width: screenWidth - 64,
+		height: 81,
+		padding: 22,
+		borderRadius: 16,
+		marginBottom: 50
+	},
+	button_title: {
+		fontFamily: "Poppins_400",
+		fontSize: 24,
+		color: colors.text
+	}
+});
