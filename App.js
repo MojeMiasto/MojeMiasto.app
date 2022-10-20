07c4348a5655f3393ca25ad6e9e26591e875f1a8
ShakingDonut <kacper.siembab@gmail.com>
2022-10-20 14:31:13 +0200

    unused import

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import * as SplashScreen from "expo-splash-screen";
 import AnimatedSplash from "react-native-animated-splash-screen";
 
 import "./src/IMLocalize";
