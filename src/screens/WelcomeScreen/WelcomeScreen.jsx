4c3ddfa5570b0e0b3d2869f97d5dd8bfa25617fe
Kamil Augustak <kamil.augustak@gmail.com>
2022-10-20 22:33:30 +0200

    notification implementation (not finished)

--- src/screens/WelcomeScreen/WelcomeScreen.jsx
+++ src/screens/WelcomeScreen/WelcomeScreen.jsx
@@ -3,12 +3,24 @@ import Background from "../../components/Background/Background";
 import { useTranslation } from "react-i18next";
 
 import { screenWidth } from "../../styles";
-
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import style from "./styles_WelcomeScreen";
 import WelcomeButton from "./WelcomeButton";
+import {useEffect} from "react";
+import {registerForPushNotificationsAsync} from "../../notifications/notificationsRepository";
 
 export default function WelcomeScreen({ navigation }) {
 	const { t } = useTranslation();
+
+	useEffect(() => {
+		const configure = async () => {
+			const token = await registerForPushNotificationsAsync()
+			AsyncStorage.setItem("notificationToken", token);
+			console.log(token);
+		}
+		configure()
+	}, [])
+
 	const onPressHandler = () => {
 		navigation.navigate("Location");
 	};
