e3f9a8ce1e4d65086102c720c1de857329211bc0
Kamil Augustak <kamil.augustak@gmail.com>
2022-10-22 00:44:54 +0200

    font size fix

--- src/components/NextWasteCard/NextWasteCard.jsx
+++ src/components/NextWasteCard/NextWasteCard.jsx
@@ -2,17 +2,38 @@ import { View, Text, Image } from "react-native";
 import wastebin from "../../assets/img/wastebin.png";
 import style from "./styles_NextWasteCard.js";
 import moment from "moment";
+import {useState} from "react";
 
 export default function NextWasteCard({ wasteType, wasteDate }) {
 	moment.locale("pl");
 	const date = moment(wasteDate).format("dddd, D MMMM YYYY");
+	const AdjustLabel = ({
+							 fontSize, text, style, numberOfLines
+						 }) => {
+		const [currentFont, setCurrentFont] = useState(fontSize);
 
+		return (
+			<Text
+				numberOfLines={ numberOfLines }
+				adjustsFontSizeToFit={true}
+				style={ [style, { fontSize: currentFont }] }
+				onTextLayout={ (e) => {
+					const { lines } = e.nativeEvent;
+					if (lines.length > numberOfLines) {
+						setCurrentFont(currentFont - 1);
+					}
+				} }
+			>
+				{ text }
+			</Text>
+		);
+	};
 	return (
 		<View style={style.container}>
 			<Image source={wastebin} />
 			<View style={style.content}>
 				<Text style={style.date}>{date}</Text>
-				<Text style={style.title}>{wasteType}</Text>
+				{AdjustLabel({fontSize: style.title.fontSize, text: wasteType, style: style.title, numberOfLines: 2})}
 			</View>
 		</View>
 	);
