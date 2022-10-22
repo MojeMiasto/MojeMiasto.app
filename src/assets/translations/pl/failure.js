e81686571fc0b995491dacecd1bb47ae98f7c039
Kacper <kacper.siembab@gmail.com>
2022-10-22 22:15:56 +0200

    added fetching of failures and translations

---
+++ src/assets/translations/pl/failure.js
@@ -0,0 +1,6 @@
+export default {
+	outage_title: "Planowane wyłączenia",
+	outage_empty: "Brak planowanych wyłączeń",
+	failure_title: "Awarie",
+	failure_empty: "Brak awarii"
+};
--- src/assets/translations/pl/index.js
+++ src/assets/translations/pl/index.js
@@ -4,6 +4,7 @@ import waste from "./waste";
 import welcome from "./welcome";
 import location from "./location";
 import pollution from "./pollution";
+import failure from "./failure";
 
 export default {
 	common,
@@ -11,5 +12,6 @@ export default {
 	waste,
 	welcome,
 	location,
-	pollution
+	pollution,
+	failure
 };
