20e4ef2a7e910a9b7e7db126f595a8c524319739
Kacper <kacper.siembab@gmail.com>
2022-10-22 16:57:18 +0200

    screenoptions

--- src/screenOptions.js
+++ src/screenOptions.js
@@ -14,6 +14,8 @@ export const screenOptions = ({ route }) => ({
 			iconName = focused ? "ios-warning" : "ios-warning-outline";
 		} else if (route.name === "Pollution") {
 			return <Feather name="wind" size={24} color={color} />;
+		} else if (route.name === "Receipts") {
+			iconName = focused ? "ios-receipt" : "ios-receipt-outline";
 		}
 
 		return <Ionicons name={iconName} size={24} color={color} />;
